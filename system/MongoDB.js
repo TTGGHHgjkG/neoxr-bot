dac20348a17a8072990ad969b1ea89ebc37b29f3
KiZakiXD <88037146+KiZakiXD@users.noreply.github.com>
2024-05-27 19:40:26 +0700

    Update system

---
+++ system/MongoDB.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+module.exports = class MongoDB {
+      options = {
+         useNewUrlParser: true,
+         useUnifiedTopology: true
+      }
+      connection = process.env.DATABASE_URL
+      model = {
+         database: {}
+      }
+      data = {}
+      
+      fetch = async () => {
+         mongoose.connect(this.connection, {
+            ...this.options
+         })
+         try {
+            const schemaData = new mongoose.Schema({
+               data: {
+                  type: Object,
+                  required: true,
+                  default: {}
+               }
+            })
+            this.model.database = mongoose.model('data', schemaData)
+         } catch {
+            this.model.database = mongoose.model('data')
+         }
+         this.data = await this.model.database.findOne({})
+         if (!this.data) {
+            (new this.model.database({
+               data: {}
+            })).save()
+            this.data = await this.model.database.findOne({})
+            return this.data.data
+         } else return this.data.data
+      }
+
+      save = async (data) => {
+         const obj = data ? data : global.db
+         if (this.data && !this.data.data) return (new this.model.database({
+            data: obj
+         })).save()
+         this.model.database.findById(this.data._id, (error, document) => {
+            if (error) return
+            if (!document.data) document.data = {}
+            document.data = global.db
+            document.save()
+         })
+      }
+   }
